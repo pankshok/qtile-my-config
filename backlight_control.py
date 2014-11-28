2df1f37f5dbb6fda9f5428056246895d632cd14c
pankshok <kulyov.pavel@gmail.com>
2014-11-28 20:42:50 +0300

    add documentation string

--- backlight_control.py
+++ backlight_control.py
@@ -1,14 +1,34 @@
-#TODO Wrap all xbacklight arguments
 #TODO Combine this control class with widget that will show current backlight value
 #TODO Replase subprocess.call with qtile apropriate command
 import subprocess
 
 class BacklightControl(object):
-    def __init__(self, 
+    '''Class for controlling backlight with xbacklight(optional) 
+    with qtile window manager(optional too)
+
+    1) Create instance
+    2) Call increase and decrease functions
+
+    Arguments:
+    :command_get - command that executes to get current backlight value
+    :command_set - command that executes to set new backlight value
+                   MUST have {val}
+    :default_backlight - sets on instantiantion
+    :step - changes backlight by that value
+    :min - minimal value for backlight (%)
+    :max - maximal value for backlight (%)
+    :parse_function - function for parsing result of command_get execution
+                      MUST take string and return integer
+    '''
+
+    def __init__(self,
             command_get="xbacklight -set",
             command_set="xbacklight -set {val}",
             default_backlight=60,
-            step=10, max=100, min=10, parse_function=lambda s: int(s)):
+            step=10,
+            max=100,
+            min=10,
+            parse_function=lambda s: int(s)):
 
         self._command_get = command_get
         self._command_set = command_set
